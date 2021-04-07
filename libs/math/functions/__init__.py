0f642a7cbe772c1ba37daea63f385d44fd9a3c57
Omykronn <omykronn.player@gmail.com>
2021-04-07 21:38:08 +0200

    Recreation of functions\__init__.py

---
+++ libs/math/functions/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Mathematical Functions Library
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Basic Library for Derivatives of Simple Functions
+
+:copyright: (c) 2019-2021 The RaidCode
+:license: Mozilla Public License 2.0, see LICENSE for more details.
+
+"""
+
+__title__ = "math/functions"
+__author__ = "The RaidCode"
+__license__ = "Mozilla Public License 2.0"
+__copyright__ = "Copyright 2019-2021 The RaidCode"
+__version__ = "1.1.1"
